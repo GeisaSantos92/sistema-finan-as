8ab271538bce46860f6effa4b70d8cb415b6c26b
GeisaSouzaSantos <109676825+GeisaSouzaSantos@users.noreply.github.com>
2022-10-19 18:31:00 -0300

    third commit

---
+++ src/data/items.ts
@@ -0,0 +1,10 @@
+import {Item} from '../types/Item';
+
+export const items: Item[] = [
+    
+    {date: new Date(2022, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    {date: new Date(2022, 9, 15), category: 'food', title: 'BK', value: 28.90},
+    {date: new Date(2022, 9, 16), category: 'rent', title: 'Aluguel', value: 615.34},
+    {date: new Date(2022, 8, 18), category: 'salary', title: 'Salário', value: 2000.30}
+    
+]
